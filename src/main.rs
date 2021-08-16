682de2c242e7474815f9ad3c64664cc285276ded
Aiden McClelland <me@drbonez.dev>
2021-08-16 16:52:44 -0600

    async and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::convert::TryInto;
+use std::sync::Arc;
 
-use ruma::{
-    api::{OutgoingRequestAppserviceExt, SendAccessToken},
-    events::{room::message::MessageEventContent, AnyMessageEventContent},
-    RoomId, UserId,
-};
+use anyhow::{anyhow, Error};
+use ruma::api::{OutgoingRequestAppserviceExt, SendAccessToken};
+use ruma::events::room::message::MessageEventContent;
+use ruma::events::AnyMessageEventContent;
+use ruma::{RoomId, UserId};
 use serde::Deserialize;
+use serde_json::Value;
 
 #[derive(Deserialize)]
 struct Config {
@@ -17,45 +19,62 @@ struct Config {
     interval: f64,
 }
 
-fn main() {
-    let config: Config = serde_yaml::from_reader(
-        &std::fs::File::open(
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let config: Config = serde_yaml::from_str(
+        &tokio::fs::read_to_string(
             std::env::args()
                 .skip(1)
                 .next()
                 .unwrap_or_else(|| "/etc/health-check-bot/config.yaml".to_owned()),
         )
-        .unwrap(),
-    )
-    .unwrap();
+        .await?,
+    )?;
+    let config = Arc::new(config);
     loop {
         for health_check in &config.health_checks {
-            if let Err(e) =
-                reqwest::blocking::get(health_check).and_then(|res| res.error_for_status())
-            {
-                reqwest::blocking::Client::new()
-                    .execute(
-                        ruma::api::client::r0::message::send_message_event::Request::new(
-                            &config.room_id,
-                            &base32::encode(
-                                base32::Alphabet::RFC4648 { padding: false },
-                                &rand::random::<[u8; 8]>()[..],
-                            ),
-                            &AnyMessageEventContent::RoomMessage(MessageEventContent::text_plain(
-                                format!("HEALTH CHECK {} FAILED: {}", health_check, e),
-                            )),
-                        )
-                        .try_into_http_request_with_user_id::<Vec<u8>>(
-                            &config.base_url,
-                            SendAccessToken::IfRequired(&config.access_token),
-                            config.user_id.clone(),
-                        )
-                        .unwrap()
-                        .try_into()
-                        .unwrap(),
-                    )
-                    .unwrap();
-            }
+            let cfg = config.clone();
+            let health_check = health_check.clone();
+            tokio::spawn(async move {
+                if let Err(e) = reqwest::get(&health_check)
+                    .await
+                    .and_then(|res| res.error_for_status())
+                {
+                    if let Err(e) = (|| async move {
+                        let res = reqwest::Client::new()
+                            .execute(
+                                ruma::api::client::r0::message::send_message_event::Request::new(
+                                    &cfg.room_id,
+                                    &base32::encode(
+                                        base32::Alphabet::RFC4648 { padding: false },
+                                        &rand::random::<[u8; 8]>()[..],
+                                    ),
+                                    &AnyMessageEventContent::RoomMessage(
+                                        MessageEventContent::text_plain(format!(
+                                            "HEALTH CHECK {} FAILED: {}",
+                                            health_check, e
+                                        )),
+                                    ),
+                                )
+                                .try_into_http_request_with_user_id::<Vec<u8>>(
+                                    &cfg.base_url,
+                                    SendAccessToken::IfRequired(&cfg.access_token),
+                                    cfg.user_id.clone(),
+                                )?
+                                .try_into()?,
+                            )
+                            .await?;
+                        if let Err(e) = res.error_for_status_ref() {
+                            return Err(anyhow!("{}: {}", e, res.json::<Value>().await?));
+                        }
+                        Ok::<(), Error>(())
+                    })()
+                    .await
+                    {
+                        eprintln!("ERROR SENDING MATRIX MESSAGE: {}", e);
+                    }
+                }
+            });
             std::thread::sleep(std::time::Duration::from_secs_f64(config.interval))
         }
     }
